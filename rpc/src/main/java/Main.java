8f5ea530d50f675c0e619c3d0a179ef961b11da5
liujiayu03 <15822882820@163.com>
2021-05-05 14:51:04 +0800

    first add

---
+++ rpc/src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author liujiayu03 <liujiayu03@kuaishou.com>
+ * Created on 2021-05-05
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
